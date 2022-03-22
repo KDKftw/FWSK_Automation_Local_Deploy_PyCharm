53b1076cca8f2086f7eda717263c197d6e579496
KDKftw <ooo.kadoun@gmail.com>
2022-03-22 21:37:22 +0100

    no message

--- starter_local.py
+++ starter_local.py
@@ -55,8 +55,8 @@ if __name__ == '__main__':
     #outfile = open("C:\Users\KDK\Desktop\HTML_TEST_REPORTS\sest_results.html", "w")
     outfile = open("results.html", "w")
 
-    runner = HtmlTestRunner.HTMLTestRunner(log=True, verbosity=2, output='report', title='FISCHER WEB Suite Report', report_name='FISCHER WEB Suite Report',
-                            open_in_browser=True, description="FISCHER WEB Suite Report")
+    runner = HtmlTestRunner.HTMLTestRunner(log=True, verbosity=2, output='report', title='FISCHER SK WEB Suite Report', report_name='FISCHER SK WEB Suite Report',
+                            open_in_browser=True, description="FISCHER SK WEB Suite Report")
     #runner = HTMLTestRunner.HTMLTestRunner(log=True, verbosity=2, output='report', title='FISCHER WEB Suite Report', report_name='FISCHER WEB Suite Report', open_in_browser=True, description="FISCHER WEB Suite Report")
     ##at office PC gotta be set up like that (???)
     runner.run(suite())
